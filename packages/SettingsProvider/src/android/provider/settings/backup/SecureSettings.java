41ff8f9143b90c0eaf21439a22219f2fc4db6207
Nico60 <ndhouailly@gmail.com>
2016-05-16 21:48:30 +0200

    Add animations to quick settings tiles [1/2]
    Change-Id: I81e79cfccfc4565b1f7a6292b5f0b88be753df74
    [ jaysonedson: Brought from nougat->android12 ]
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -259,6 +259,8 @@ public class SecureSettings {
         Settings.Secure.EVEN_DIMMER_ACTIVATED,
         Settings.Secure.EVEN_DIMMER_MIN_NITS,
         Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS,
-        Settings.Secure.DOZE_ON_CHARGE
+        Settings.Secure.DOZE_ON_CHARGE,
+        Settings.Secure.QUICK_SETTINGS_TILES_ANIM_STYLE,
+        Settings.Secure.QUICK_SETTINGS_TILES_ANIM_DURATION
     };
 }

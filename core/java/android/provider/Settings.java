9e11a076ec312adc891917bceff0fdc0d3cb9730
Sam Mortimer <sam@mortimer.me.uk>
2017-10-13 13:19:53 -0700

    Settings: Add back option to pass swipe-up-to-unlock
    This got removed unintentionally by:
      Author: Sam Mortimer <sam@mortimer.me.uk>
      Date:   Wed Oct 11 23:00:18 2017 -0700
          Remove ThirdPartyKeyguard
          Change-Id: Ie3717b9ea7a23988ebd646ed7e2fe8f105a4a693
    
    Add back the lock util methods from the original commit:
      Author: Danesh M <daneshm90@gmail.com>
      Date:   Mon Feb 8 14:57:31 2016 -0800
          CMSettings : Add option to pass swipe-up-to-unlock (3/3)
          CYNGNOS-1873
          Change-Id: Icdd2505dbc56a77f49a76ea61daf75c5d8a24fd6
    
    Change-Id: I6177700be955f42505dbd5fbb23461f8e151e1b4

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13324,6 +13324,12 @@ public final class Settings {
         public static final String WINDOW_IGNORE_SECURE = "window_ignore_secure";
 
         /**
+         * Whether keyguard will direct show security view (0 = false, 1 = true)
+         * @hide
+         */
+        public static final String LOCK_PASS_TO_SECURITY_VIEW = "lock_screen_pass_to_security_view";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */

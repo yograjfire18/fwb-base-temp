545ba5e433ef3ca7b05c5d0eaf343a1186c4656f
ezio84 <brabus84@gmail.com>
2018-09-11 04:08:01 -0500

    derpUtils: Add method to turn off the screen
    Change-Id: Ie65d8ebe354996ae8d67ac950d635a1169830931
    Signed-off-by: Saikiran <bjsaikiran@gmail.com>

--- core/java/com/android/internal/util/derp/derpUtils.java
+++ core/java/com/android/internal/util/derp/derpUtils.java
@@ -22,8 +22,10 @@ import android.content.res.Resources;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.os.SystemClock;
 
 import com.android.internal.statusbar.IStatusBarService;
 
@@ -34,6 +36,15 @@ import java.util.Locale;
  * @hide
  */
 public class derpUtils {
+
+    // Method to turn off the screen
+    public static void switchScreenOff(Context ctx) {
+        PowerManager pm = (PowerManager) ctx.getSystemService(Context.POWER_SERVICE);
+        if (pm!= null) {
+            pm.goToSleep(SystemClock.uptimeMillis());
+        }
+    }
+
     public static boolean isPackageInstalled(Context context, String packageName, boolean ignoreState) {
         if (packageName != null) {
             try {

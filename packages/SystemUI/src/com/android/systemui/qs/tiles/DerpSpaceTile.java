b94ce36dd0480581d7e88c9d6bcfdcb1d298966e
Ali B <abittin@gmail.com>
2020-10-25 18:45:10 +0300

    SystemUI: Add DerpSpace Tile
    Bring it back to O
    
    Include this modifed patch:
    http://gerrit.aicp-rom.com/#/c/35017/
    
    Bring it up to Q and adapt it to the new
    OTA Updater package and activity
    
    Ported to R & S
    
    Change-Id: Icec3000dd7249001bcc65810315fe42489779466
    [nurkeinneid - Update to Android 14]
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

---
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/DerpSpaceTile.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright (C) 2015 The Dirty Unicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Context;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.derp.derpUtils;
+import com.android.systemui.SysUIToast;
+import com.android.systemui.res.R;
+import com.android.systemui.dagger.qualifiers.Background;
+import com.android.systemui.dagger.qualifiers.Main;
+import com.android.systemui.plugins.ActivityStarter;
+import com.android.systemui.plugins.FalsingManager;
+import com.android.systemui.plugins.qs.QSIconView;
+import com.android.systemui.plugins.qs.QSTile;
+import com.android.systemui.plugins.qs.QSTile.State;
+import com.android.systemui.plugins.statusbar.StatusBarStateController;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.QsEventLogger;
+import com.android.systemui.qs.logging.QSLogger;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+
+import javax.inject.Inject;
+
+public class DerpSpaceTile extends QSTileImpl<State> {
+
+    public static final String TILE_SPEC = "derpspace";
+
+    private boolean mListening;
+    private final ActivityStarter mActivityStarter;
+
+    private static final String TAG = "DerpSpaceTile";
+
+    private static final String DERPSPACE_PKG_NAME = "com.android.settings";
+    private static final String OTA_PKG_NAME = "org.lineageos.updater";
+
+    private static final Intent DERPSPACE_INTENT = new Intent()
+            .setComponent(new ComponentName(DERPSPACE_PKG_NAME,
+                "com.android.settings.Settings$DerpSpaceSettingsActivity"));
+    private static final Intent OTA_INTENT = new Intent()
+            .setComponent(new ComponentName(OTA_PKG_NAME,
+                "org.lineageos.updater.UpdatesActivity"));
+
+    @Inject
+    public DerpSpaceTile(
+            QSHost host,
+            QsEventLogger uiEventLogger,
+            @Background Looper backgroundLooper,
+            @Main Handler mainHandler,
+            FalsingManager falsingManager,
+            MetricsLogger metricsLogger,
+            StatusBarStateController statusBarStateController,
+            ActivityStarter activityStarter,
+            QSLogger qsLogger
+    ) {
+        super(host, uiEventLogger, backgroundLooper, mainHandler, falsingManager, metricsLogger,
+                statusBarStateController, activityStarter, qsLogger);
+        mActivityStarter = activityStarter;
+    }
+
+    @Override
+    public State newTileState() {
+        return new State();
+    }
+
+    @Override
+    protected void handleClick(@Nullable View view) {
+        startDerpSpace();
+        refreshState();
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        if (isOTABundled()) {
+            return OTA_INTENT;
+        }
+        showNotSupportedToast();
+        return null;
+    }
+
+    @Override
+    protected void handleSecondaryClick(@Nullable View view) {
+        if (isOTABundled()) {
+            startDerpFestOTA();
+        }
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_derpspace_label);
+    }
+
+    protected void startDerpSpace() {
+        mActivityStarter.postStartActivityDismissingKeyguard(DERPSPACE_INTENT, 0);
+    }
+
+    protected void startDerpFestOTA() {
+        mActivityStarter.postStartActivityDismissingKeyguard(OTA_INTENT, 0);
+    }
+
+    private void showNotSupportedToast() {
+        // Collapse the panels, so the user can see the toast.
+        SysUIToast.makeText(mContext, mContext.getString(
+                R.string.quick_derpspace_toast),
+                Toast.LENGTH_LONG).show();
+    }
+
+    private boolean isOTABundled() {
+        return derpUtils.isPackageAvailable(mContext, OTA_PKG_NAME);
+    }
+
+    private boolean isDERPSPACEAvailable() {
+        boolean isInstalled = false;
+        boolean isNotHidden = false;
+        isInstalled = derpUtils.isPackageInstalled(mContext, DERPSPACE_PKG_NAME);
+        isNotHidden = derpUtils.isPackageAvailable(mContext, DERPSPACE_PKG_NAME);
+        return isInstalled || isNotHidden;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return isDERPSPACEAvailable();
+    }
+
+    @Override
+    protected void handleUpdateState(State state, Object arg) {
+        state.icon = ResourceIcon.get(R.drawable.ic_qs_derpspace);
+        state.label = mContext.getString(R.string.quick_derpspace_label);
+    }
+
+    @Override
+    public void handleSetListening(boolean listening) {
+        if (mListening == listening) return;
+        mListening = listening;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.DERP;
+    }
+}

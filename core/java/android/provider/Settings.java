543ab2eda7cb4c1f73865bac38590bea1b130be0
StarkDroid <gamerprince.exp@gmail.com>
2021-06-14 16:44:17 +0200

    QS Kill button: Remove dependency for toggle
    Its not really a view obstruction of any sort, Dont really wanna spam our settings with 69 toggles for nothing
    
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    Change-Id: I87d81ae34b1018c289440bba30cce3429673a930

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6923,13 +6923,6 @@ public final class Settings {
         public static final String BACK_GESTURE_HAPTIC = "back_gesture_haptic";
 
         /**
-         * Whether to show the kill app button in notification guts
-         * @hide
-         */
-        public static final String NOTIFICATION_GUTS_KILL_APP_BUTTON =
-                "notification_guts_kill_app_button";
-
-        /**
          * Whether to show floating rotation button
          * @hide
          */

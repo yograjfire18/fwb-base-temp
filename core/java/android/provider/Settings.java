387f5668a11aaaf7f11b6befa34d6571a5bf6461
jhonboy121 <alfredmathew05@gmail.com>
2022-01-17 21:20:04 +0530

    base: allow disabling privacy indicators [1/2]
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13446,6 +13446,24 @@ public final class Settings {
                 "qs_tiles_toggleable_on_lock_screen";
 
         /**
+         * Whether to show privacy indicator for location
+         * @hide
+         */
+        public static final String ENABLE_LOCATION_PRIVACY_INDICATOR = "enable_location_privacy_indicator";
+
+        /**
+         * Whether to show privacy indicator for camera
+         * @hide
+         */
+        public static final String ENABLE_CAMERA_PRIVACY_INDICATOR = "enable_camera_privacy_indicator";
+
+        /**
+         * Whether to show privacy indicator for media projection
+         * @hide
+         */
+        public static final String ENABLE_PROJECTION_PRIVACY_INDICATOR = "enable_projection_privacy_indicator";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *

1233258356f60782569b7c9e3426edba98bf0afa
Pranav Vashi <neobuddy89@gmail.com>
2022-01-30 00:14:20 +0530

    Forward port pixel navbar animation from redfin 11 [1/2]
    neobuddy89:
    * This is stripped down version of animation only from Google Assist port by jhenrique.
    * Adapted for Android 12.
    * History commits and fixes can be viewed here:
      https://github.com/crdroidandroid/android_frameworks_base/commits/11.0/packages/SystemUI/src/com/google/android
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7199,6 +7199,12 @@ public final class Settings {
         public static final String ISLAND_NOTIFICATION_NOW_PLAYING = "island_notification_now_playing";
 
         /**
+         * Whether to enable the pixel navbar animation
+         * @hide
+         */
+        public static final String PIXEL_NAV_ANIMATION = "pixel_nav_animation";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *

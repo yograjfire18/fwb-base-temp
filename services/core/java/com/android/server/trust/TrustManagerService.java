b329a8d72e09237000d5906ac7ea1f48de89b85e
jhonboy121 <alfredmathew05@gmail.com>
2022-02-01 19:54:15 +0530

    base: Introduce app lock [1/4]
    base: applock: refactor
    
    * encapsulate config data sources and add a data class for per app config
    * added support for migrating between different versions of data
    * fixed notification content not being redacted.
    
    services: applock: allow background authentication
    
    base: merge hidden app feature with applock
    
    services: AppLockConfig: fix a derp in hidePackage()
    
    services: applock: fix data migration and change secure notification parameter name
    
    * also fixed addPackage replacing data without checking if entry already exists
    
    services: AppLockManagerService: use the og calling uid for target intent
    
    * fixes launching app activites not exported
    
    * nift4: avoid changing public api
    
    * base: Add system app whitelist for applock
    
    * fwb: Rename app lock permission
    
    * Dhina17 - Adapt for A14, A14 QPR2
    
    Change-Id: I36e20fac9cf3032010ae3a161ecb08638cadd002

--- services/core/java/com/android/server/trust/TrustManagerService.java
+++ services/core/java/com/android/server/trust/TrustManagerService.java
@@ -79,6 +79,7 @@ import com.android.internal.infra.AndroidFuture;
 import com.android.internal.util.DumpUtils;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.server.SystemService;
+import com.android.server.app.AppLockManagerServiceInternal;
 import com.android.server.companion.virtual.VirtualDeviceManagerInternal;
 
 import org.xmlpull.v1.XmlPullParser;
@@ -259,6 +260,8 @@ public class TrustManagerService extends SystemService {
             return mLooper;
         }
     }
+
+    private AppLockManagerServiceInternal mAppLockManagerService = null;
 
     public TrustManagerService(Context context) {
         this(context, new Injector(new LockPatternUtils(context), Looper.myLooper()));
@@ -855,7 +858,15 @@ public class TrustManagerService extends SystemService {
             }
 
             setDeviceLockedForUser(id, deviceLocked);
+            getAppLockManagerService().notifyDeviceLocked(deviceLocked, id);
         }
+    }
+
+    private AppLockManagerServiceInternal getAppLockManagerService() {
+        if (mAppLockManagerService == null) {
+            mAppLockManagerService = LocalServices.getService(AppLockManagerServiceInternal.class);
+        }
+        return mAppLockManagerService;
     }
 
     private void setDeviceLockedForUser(@UserIdInt int userId, boolean locked) {

4d33484ca90dd5a2f811907f9db9423e9281caef
cjh1249131356 <cjh1249131356@gmail.com>
2022-04-30 19:58:36 +0800

    Carrier Label: Improve code
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7128,6 +7128,12 @@ public final class Settings {
         public static final String CUSTOM_CARRIER_LABEL = "custom_carrier_label";
 
         /**
+         * Whether to enable custom carrier label
+         * @hide
+         */
+        public static final String ENABLE_CUSTOM_CARRIER_LABEL = "enable_custom_carrier_label";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate

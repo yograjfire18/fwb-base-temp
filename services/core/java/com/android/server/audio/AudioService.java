3a04298a5f19745c9ef9bbb7e86907942dbf0d7c
cjh1249131356 <cjh1249131356@gmail.com>
2022-05-26 00:08:10 +0800

    AudioService: Do not touch ringer mode on volume change when device has alert slider
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -1017,6 +1017,8 @@ public class AudioService extends IAudioService.Stub
     @GuardedBy("mSettingsLock")
     private boolean mRttEnabled = false;
 
+    private boolean mHasAlertSlider;
+
     private AtomicBoolean mMasterMute = new AtomicBoolean(false);
 
     private DisplayManager mDisplayManager;
@@ -1128,6 +1130,9 @@ public class AudioService extends IAudioService.Stub
 
         mUseVolumeGroupAliases = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_handleVolumeAliasesUsingVolumeGroups);
+
+        mHasAlertSlider = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_hasAlertSlider);
 
         // Initialize volume
         // Priority 1 - Android Property
@@ -3883,6 +3888,11 @@ public class AudioService extends IAudioService.Stub
     private int getNewRingerMode(int stream, int index, int flags) {
         // setRingerMode does nothing if the device is single volume,so the value would be unchanged
         if (mIsSingleVolume) {
+            return getRingerModeExternal();
+        }
+
+        // Do not change ringer mode when device has Alert Slider
+        if (mHasAlertSlider) {
             return getRingerModeExternal();
         }
 

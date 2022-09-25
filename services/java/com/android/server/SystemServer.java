23e092504295b14b0c7fa2c4cd6f9851c2f50f11
cjh1249131356 <cjh1249131356@gmail.com>
2022-09-25 14:50:31 +0800

    base: Add mock oplus LinearmotorVibratorService
    * To get haptic feedback in OnePlus / Oplus Camera.
    
    Squashed with:
    
        Author: Pranav Vashi <neobuddy89@gmail.com>
        Date: Sun, 28 May 2023 10:36:44 +0530
        Subject: base: Update mock LinearmotorVibratorService
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        Author: Pranav Vashi <neobuddy89@gmail.com>
        Date: Fri, 26 May 2023 00:00:42 +0530
        Subject: base: Add overlay to mock oplus LinearmotorVibratorService
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    [neobuddy89: Remove unnecessary bits.]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -229,6 +229,7 @@ import com.android.server.twilight.TwilightService;
 import com.android.server.uri.UriGrantsManagerService;
 import com.android.server.usage.UsageStatsService;
 import com.android.server.utils.TimingsTraceAndSlog;
+import com.android.server.vibrator.LinearmotorVibratorService;
 import com.android.server.vibrator.VibratorManagerService;
 import com.android.server.vr.VrManagerService;
 import com.android.server.wearable.WearableSensingManagerService;
@@ -2705,6 +2706,13 @@ public final class SystemServer implements Dumpable {
             t.traceBegin("StartBackgroundInstallControlService");
             mSystemServiceManager.startService(BackgroundInstallControlService.class);
             t.traceEnd();
+
+            if (context.getResources().getBoolean(
+                    com.android.internal.R.bool.config_mockOplusLinearmotorVibratorService)) {
+                t.traceBegin("StartLinearmotorVibratorService");
+                mSystemServiceManager.startService(LinearmotorVibratorService.class);
+                t.traceEnd();
+            }
 
             // LineageHardware
             t.traceBegin("StartLineageHardwareService");

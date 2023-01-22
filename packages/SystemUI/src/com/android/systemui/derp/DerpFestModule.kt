1d8d826ee56bc5066a493f672345a6b9514bb744
SpiritCroc <dev@spiritcroc.de>
2023-01-22 23:11:14 +0300

    QS: Locale Tile
    Toggle the primary default language
    
    - Bring it to Oreo - LorDClockaN
    - Bring it to Pie, 11, 12 - eyosen
    - Bring it to T - semdoc
    
    Change-Id: I3e336eddb360ea1a796727657652ad9301007380

--- packages/SystemUI/src/com/android/systemui/derp/DerpFestModule.kt
+++ packages/SystemUI/src/com/android/systemui/derp/DerpFestModule.kt
@@ -24,6 +24,7 @@ import com.android.systemui.qs.tiles.CellularTile
 import com.android.systemui.qs.tiles.CompassTile
 import com.android.systemui.qs.tiles.DataSwitchTile
 import com.android.systemui.qs.tiles.HeadsUpTile
+import com.android.systemui.qs.tiles.LocaleTile
 import com.android.systemui.qs.tiles.PowerShareTile
 import com.android.systemui.qs.tiles.ReadingModeTile
 import com.android.systemui.qs.tiles.ScreenshotTile
@@ -83,6 +84,12 @@ interface DerpFestModule {
     @IntoMap
     @StringKey(HeadsUpTile.TILE_SPEC)
     fun bindHeadsUpTile(headsUpTile: HeadsUpTile): QSTileImpl<*>
+
+    /** Inject LocaleTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(LocaleTile.TILE_SPEC)
+    fun bindLocaleTile(localeTile: LocaleTile): QSTileImpl<*>
 
     /** Inject PowerShareTile into tileMap in QSModule */
     @Binds

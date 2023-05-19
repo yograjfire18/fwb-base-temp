b7b398b23f8c7fbee09c392179ff01cbea65c90f
nift4 <nift4@protonmail.com>
2023-05-19 16:34:56 +0200

    SystemUI: bring back clipboard navbar button
    * Using new navbar drag-n-drop APIs, this works with 3-button and
      2-button navigation
    
    Change-Id: I8ed8cf0857a36f654d8b15db395fd4a3b1b52c74

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
@@ -1718,6 +1718,9 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
         if (mView.getVolumePlusButton() != null) {
             updateButtonLocation(region, touchRegionCache, mView.getVolumePlusButton(), inScreenSpace, useNearestRegion);
         }
+        if (mView.getClipboardButton() != null) {
+            updateButtonLocation(region, touchRegionCache, mView.getClipboardButton(), inScreenSpace, useNearestRegion);
+        }
         return region;
     }
 

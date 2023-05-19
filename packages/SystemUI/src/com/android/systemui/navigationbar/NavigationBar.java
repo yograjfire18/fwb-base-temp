aff7a204b87b1bf6b9e9ea6c7f58cca0d34de105
nift4 <nift4@protonmail.com>
2023-05-19 20:26:46 +0200

    Porting menu button from pie to new pipeline
    Change-Id: Ice9d7eaf5b4067bec32af52820e9f80385200948

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
@@ -184,6 +184,7 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
     private static final String EXTRA_APPEARANCE = "appearance";
     private static final String EXTRA_BEHAVIOR = "behavior";
     private static final String EXTRA_TRANSIENT_STATE = "transient_state";
+    private static final String EXTRA_NEEDS_MENU = "needs_menu";
 
     /** Allow some time inbetween the long press for back and recents. */
     private static final int LOCK_TO_APP_GESTURE_TOLERANCE = 200;
@@ -234,6 +235,7 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
     private int mDisabledFlags1;
     private int mDisabledFlags2;
     private long mLastLockToAppLongPress;
+    private boolean mNeedsMenu = false;
 
     private Locale mLocale;
     private int mLayoutDirection;
@@ -687,6 +689,7 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
             mAppearance = mSavedState.getInt(EXTRA_APPEARANCE, 0);
             mBehavior = mSavedState.getInt(EXTRA_BEHAVIOR, 0);
             mTransientShown = mSavedState.getBoolean(EXTRA_TRANSIENT_STATE, false);
+            mNeedsMenu = mSavedState.getBoolean(EXTRA_NEEDS_MENU, false);
         }
 
         // Respect the latest disabled-flags.
@@ -795,6 +798,7 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
                 ? mMainAutoHideController : mAutoHideControllerFactory.create(mContext);
         setAutoHideController(autoHideController);
         restoreAppearanceAndTransientState();
+        mView.setMenuVisibility(mNeedsMenu);
     }
 
     @Override
@@ -826,6 +830,7 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
         outState.putInt(EXTRA_APPEARANCE, mAppearance);
         outState.putInt(EXTRA_BEHAVIOR, mBehavior);
         outState.putBoolean(EXTRA_TRANSIENT_STATE, mTransientShown);
+        outState.putBoolean(EXTRA_NEEDS_MENU, mNeedsMenu);
         getBarTransitions().getLightTransitionsController().saveState(outState);
     }
 
@@ -1008,7 +1013,7 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
     public void onSystemBarAttributesChanged(int displayId, @Appearance int appearance,
             AppearanceRegion[] appearanceRegions, boolean navbarColorManagedByIme,
             @Behavior int behavior, @InsetsType int requestedVisibleTypes, String packageName,
-            LetterboxDetails[] letterboxDetails) {
+            LetterboxDetails[] letterboxDetails, boolean needsMenu) {
         if (displayId != mDisplayId) {
             return;
         }
@@ -1025,6 +1030,10 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
             mBehavior = behavior;
             mView.setBehavior(behavior);
             updateSystemUiStateFlags();
+        }
+        if (mNeedsMenu != needsMenu) {
+            mNeedsMenu = needsMenu;
+            mView.setMenuVisibility(needsMenu);
         }
     }
 
--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
@@ -324,6 +324,8 @@ public class NavigationBarView extends FrameLayout implements TunerService.Tunab
 
         // Set up the context group of buttons
         mContextualButtonGroup = new ContextualButtonGroup(R.id.menu_container);
+        final ContextualButton menuButton = new ContextualButton(R.id.menu,
+                mLightContext, R.drawable.ic_sysbar_menu);
         final ContextualButton imeSwitcherButton = new ContextualButton(R.id.ime_switcher,
                 mLightContext, R.drawable.ic_ime_switcher_default);
         final ContextualButton accessibilityButton =
@@ -333,6 +335,7 @@ public class NavigationBarView extends FrameLayout implements TunerService.Tunab
                 mLightContext, R.drawable.ic_chevron_start);
         final ContextualButton cursorRightButton = new ContextualButton(R.id.dpad_right,
                 mLightContext, R.drawable.ic_chevron_end);
+        mContextualButtonGroup.addButton(menuButton);
         mContextualButtonGroup.addButton(imeSwitcherButton);
         mContextualButtonGroup.addButton(accessibilityButton);
         mRotationContextButton = new RotationContextButton(R.id.rotate_suggestion,
@@ -365,6 +368,7 @@ public class NavigationBarView extends FrameLayout implements TunerService.Tunab
         mButtonDispatchers.put(R.id.home, new ButtonDispatcher(R.id.home));
         mButtonDispatchers.put(R.id.home_handle, new ButtonDispatcher(R.id.home_handle));
         mButtonDispatchers.put(R.id.recent_apps, new ButtonDispatcher(R.id.recent_apps));
+        mButtonDispatchers.put(R.id.menu, menuButton);
         mButtonDispatchers.put(R.id.ime_switcher, imeSwitcherButton);
         mButtonDispatchers.put(R.id.accessibility_button, accessibilityButton);
         mButtonDispatchers.put(R.id.menu_container, mContextualButtonGroup);
@@ -480,6 +484,10 @@ public class NavigationBarView extends FrameLayout implements TunerService.Tunab
 
     public ButtonDispatcher getRecentsButton() {
         return mButtonDispatchers.get(R.id.recent_apps);
+    }
+
+    public ButtonDispatcher getMenuButton() {
+        return mButtonDispatchers.get(R.id.menu);
     }
 
     public ButtonDispatcher getBackButton() {
@@ -1005,6 +1013,10 @@ public class NavigationBarView extends FrameLayout implements TunerService.Tunab
     @Override
     public void onNavigationModeChanged(int mode) { /* Do nothing */ }
 
+    public void setMenuVisibility(final boolean show) {
+        mContextualButtonGroup.setButtonVisibility(R.id.menu, show);
+    }
+
     public void setAccessibilityButtonState(final boolean visible, final boolean longClickable) {
         mLongClickableAccessibilityButton = longClickable;
         getAccessibilityButton().setLongClickable(longClickable);
@@ -1341,9 +1353,10 @@ public class NavigationBarView extends FrameLayout implements TunerService.Tunab
                         visibilityToString(getCurrentView().getVisibility()),
                         getCurrentView().getAlpha()));
 
-        pw.println(String.format("      disabled=0x%08x vertical=%s darkIntensity=%.2f",
+        pw.println(String.format("      disabled=0x%08x vertical=%s menu=%s darkIntensity=%.2f",
                         mDisabledFlags,
                         mIsVertical ? "true" : "false",
+                        getMenuButton().isVisible() ? "true" : "false",
                         getLightTransitionsController().getCurrentDarkIntensity()));
 
         pw.println("    mScreenOn: " + mScreenOn);
@@ -1353,6 +1366,7 @@ public class NavigationBarView extends FrameLayout implements TunerService.Tunab
         dumpButton(pw, "home", getHomeButton());
         dumpButton(pw, "handle", getHomeHandle());
         dumpButton(pw, "rcnt", getRecentsButton());
+        dumpButton(pw, "menu", getMenuButton());
         dumpButton(pw, "rota", getRotateSuggestionButton());
         dumpButton(pw, "a11y", getAccessibilityButton());
         dumpButton(pw, "ime", getImeSwitchButton());

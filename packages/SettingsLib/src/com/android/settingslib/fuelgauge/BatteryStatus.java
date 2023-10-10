dc2b52dbd7fa636098f6924212564c1a3ab4e2a2
Lucchetto <lucchetto.tie@live.com>
2023-10-10 07:47:36 +0000

    Lockscreen charging info: show decimal in battery temperature
    Change-Id: I10bbe4df988387c401802475697452796e0afb6d
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SettingsLib/src/com/android/settingslib/fuelgauge/BatteryStatus.java
+++ packages/SettingsLib/src/com/android/settingslib/fuelgauge/BatteryStatus.java
@@ -62,7 +62,7 @@ public class BatteryStatus {
     public final int maxChargingWattage;
     public final boolean present;
     public final Optional<Boolean> incompatibleCharger;
-    public final int temperature;
+    public final float temperature;
 
     public static BatteryStatus create(Context context, boolean incompatibleCharger) {
         final Intent batteryChangedIntent = BatteryUtils.getBatteryIntent(context);
@@ -72,7 +72,7 @@ public class BatteryStatus {
 
     public BatteryStatus(int status, int level, int plugged, int chargingStatus,
             int maxChargingWattage, boolean present, int maxChargingCurrent, int maxChargingVoltage,
-            int temperature) {
+            float temperature) {
         this.status = status;
         this.level = level;
         this.plugged = plugged;

2709ab717a384d30d001457c4bec394acb902148
minaripenguin <minaripenguin@users.noreply.github.com>
2023-11-11 13:45:11 +0800

    PowerManager: Acquire wakelock when waking device during plug/unplug
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -361,6 +361,9 @@ public final class PowerManagerService extends SystemService
 
     private boolean mButtonLightOnKeypressOnly;
 
+    private PowerManager mPowerManager;
+    private PowerManager.WakeLock mPlugWakeLock;
+
     private final InattentiveSleepWarningController mInattentiveSleepWarningOverlayController;
     private final AmbientDisplaySuppressionController mAmbientDisplaySuppressionController;
 
@@ -1214,6 +1217,11 @@ public final class PowerManagerService extends SystemService
         mPermissionCheckerWrapper = mInjector.createPermissionCheckerWrapper();
         mPowerPropertiesWrapper = mInjector.createPowerPropertiesWrapper();
         mDeviceConfigProvider = mInjector.createDeviceConfigParameterProvider();
+
+        mPowerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        if (mPowerManager != null) {
+            mPlugWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "DeviceWakeupOnCharge");
+        }
 
         mPowerGroupWakefulnessChangeListener = new PowerGroupWakefulnessChangeListener();
 
@@ -2786,8 +2794,17 @@ public final class PowerManagerService extends SystemService
             return false;
         }
 
-        // Otherwise wake up!
-        return true;
+        try {
+             if (mPlugWakeLock != null){
+                mPlugWakeLock.acquire();
+             }
+            // Otherwise wake up!
+            return true;
+        } finally {
+            if (mPlugWakeLock != null){
+                mPlugWakeLock.release();
+            }
+        }
     }
 
     /**

aa74a909319d211ab3216b5d48b26b831c5ee533
Ali B <abittin@gmail.com>
2024-03-08 01:33:41 +0100

    CarrierLabel: Hide carrier label properly
    Change-Id: I039290dd24c9ff99721aa569d8347d6d4acbf1fc

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
@@ -817,7 +817,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
 
     public void hideCarrierName(boolean animate) {
         if (mCustomCarrierLabel != null) {
-            animateHide(mCustomCarrierLabel, animate, false);
+            animateHiddenState(mCustomCarrierLabel, View.GONE, animate);
         }
     }
 
@@ -952,7 +952,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         if (mHasCarrierLabel) {
             animateShow(mCustomCarrierLabel, animate);
         } else {
-            animateHide(mCustomCarrierLabel, animate, false);
+            animateHiddenState(mCustomCarrierLabel, View.GONE, animate);
         }
     }
 

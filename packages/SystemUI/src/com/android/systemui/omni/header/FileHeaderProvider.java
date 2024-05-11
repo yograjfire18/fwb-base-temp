eldainosor/frameworks_base-1

base: Shrink custom file headers before applying
Custom headers and new cameras with bigger resolution sizes aren't a good match.
This might put a stop to those who put a 4K image as their header and complains about SysUI struggling :)

Change-Id: I4a9ade0008f532d84160f44c57efdaa3e1fdeb30

packages/SystemUI/src/com/android/systemui/omni/header/FileHeaderProvider.java
@@ -37,6 +37,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Calendar;
 
+import com.android.internal.util.bootleg.ImageHelper;
 import com.android.systemui.R;
 
 public class FileHeaderProvider implements
@@ -115,7 +116,7 @@ public class FileHeaderProvider implements
         if (file.exists()) {
             if (DEBUG) Log.i(TAG, "Load header image");
             final Bitmap image = BitmapFactory.decodeFile(file.getAbsolutePath());
-            mImage = new BitmapDrawable(mContext.getResources(), image);
+            mImage = new BitmapDrawable(mContext.getResources(), ImageHelper.resizeMaxDeviceSize(mContext, image));
         }
     }
 

yograjfire18/fwb-base-temp

packages/SystemUI/src/com/android/systemui/omni/header/FileHeaderProvider.java
@@ -37,6 +37,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Calendar;
 
+import com.android.internal.util.derp.ImageHelper;
 import com.android.systemui.res.R;
 
 public class FileHeaderProvider implements
@@ -115,7 +116,7 @@ public class FileHeaderProvider implements
         if (file.exists()) {
             if (DEBUG) Log.i(TAG, "Load header image");
             final Bitmap image = BitmapFactory.decodeFile(file.getAbsolutePath());
-            mImage = new BitmapDrawable(mContext.getResources(), image);
+            mImage = new BitmapDrawable(mContext.getResources(), ImageHelper.resizeMaxDeviceSize(mContext, image));
         }
     }
 
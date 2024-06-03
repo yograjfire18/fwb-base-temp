Project-Kaleidoscope/android_frameworks_base

SystemUI: Enable Launcher3NoHintOverlay when in no hint mode
Change-Id: I2da73dedddcbe90c734d99b639512f4e3f7c963b
Signed-off-by: LibXZR <i@xzr.moe>

packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarInflaterView.java
@@ -94,6 +94,8 @@ public class NavigationBarInflaterView extends FrameLayout
             Settings.Secure.NAVIGATION_BAR_HINT;
     private static final String OVERLAY_NAVIGATION_HIDE_HINT =
             "ink.kaleidoscope.overlay.customization.navbar.nohint";
+    private static final String OVERLAY_LAUNCHER3_HIDE_HINT =
+            "ink.kaleidoscope.overlay.customization.launcher3.nohint";
 
     protected LayoutInflater mLayoutInflater;
     protected LayoutInflater mLandscapeInflater;
@@ -249,6 +251,7 @@ public class NavigationBarInflaterView extends FrameLayout
         final int userId = ActivityManager.getCurrentUser();
         try {
             iom.setEnabled(OVERLAY_NAVIGATION_HIDE_HINT, state, userId);
+            iom.setEnabled(OVERLAY_LAUNCHER3_HIDE_HINT, state, userId);
             if (state) {
                 // As overlays are also used to apply navigation mode, it is needed to set
                 // our customization overlay to highest priority to ensure it is applied.

yograjfire18/fwb-base-temp

packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarInflaterView.java
@@ -97,6 +97,8 @@ public class NavigationBarInflaterView extends FrameLayout implements TunerServi
             Settings.Secure.KEYBOARD_NO_NAVIGATION_BAR;
     private static final String OVERLAY_NAVIGATION_HIDE_HINT =
             "org.derpfest.overlay.customization.navbar.nohint";
+    private static final String OVERLAY_LAUNCHER3_HIDE_HINT =
+            "org.derpfest.overlay.customization.launcher3.nohint";
     private static final String OVERLAY_KEYBOARD_HIDE_NAVIGATION =
             "org.derpfest.overlay.customization.navbar.keyboard.nonavbar";
 
@@ -296,6 +298,7 @@ public class NavigationBarInflaterView extends FrameLayout implements TunerServi
         final int userId = ActivityManager.getCurrentUser();
         try {
             iom.setEnabled(OVERLAY_NAVIGATION_HIDE_HINT, state, userId);
+            iom.setEnabled(OVERLAY_LAUNCHER3_HIDE_HINT, state, userId);
             iom.setEnabled(OVERLAY_KEYBOARD_HIDE_NAVIGATION,
                     state && mIsKeyboardNavigationDisabled, userId);
             if (state) {
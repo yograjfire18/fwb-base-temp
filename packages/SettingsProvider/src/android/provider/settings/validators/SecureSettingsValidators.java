yaap/frameworks_base

base: Choose which ringer modes to cycle with the gesture [1/2]
User can still select a single mode, which disables cycling and just
applies the desired mode.

Change-Id: I6c48ae7b3fd67e85ec70124bdde42a3b2a0ed0c7

packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
@@ -171,7 +171,22 @@ public class SecureSettingsValidators {
         VALIDATORS.put(Secure.SCREENSAVER_ACTIVATE_ON_SLEEP, BOOLEAN_VALIDATOR);
         VALIDATORS.put(Secure.LOCKDOWN_IN_POWER_MENU, BOOLEAN_VALIDATOR);
         VALIDATORS.put(Secure.SHOW_FIRST_CRASH_DIALOG_DEV_OPTION, BOOLEAN_VALIDATOR);
-        VALIDATORS.put(Secure.VOLUME_HUSH_GESTURE, NON_NEGATIVE_INTEGER_VALIDATOR);
+        VALIDATORS.put(Secure.VOLUME_HUSH_GESTURE,
+                new Validator() {
+                    @Override
+                    public boolean validate(String value) {
+                        if (value.equals(Secure.YAAP_VOLUME_HUSH_OFF))
+                            return true;
+                        String[] args = value.split(",", 0);
+                        for (String str : args) {
+                            if (!str.equals(Secure.YAAP_VOLUME_HUSH_NORMAL) &&
+                                !str.equals(Secure.YAAP_VOLUME_HUSH_MUTE) &&
+                                !str.equals(Secure.YAAP_VOLUME_HUSH_VIBRATE))
+                                return false;
+                        }
+                        return true;
+                    }
+                });
         VALIDATORS.put(
                 Secure.ENABLED_NOTIFICATION_LISTENERS,
                 COLON_SEPARATED_COMPONENT_LIST_VALIDATOR); // legacy restore setting

packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
@@ -4267,8 +4267,7 @@ public class SettingsProvider extends ContentProvider {
                             Secure.VOLUME_HUSH_GESTURE);
                     if (currentSetting.isNull()) {
                         secureSettings.insertSettingOverrideableByRestoreLocked(
-                                Secure.VOLUME_HUSH_GESTURE,
-                                Integer.toString(Secure.VOLUME_HUSH_VIBRATE),
+                                Secure.VOLUME_HUSH_GESTURE, Secure.YAAP_VOLUME_HUSH_VIBRATE,
                                 null, true, SettingsState.SYSTEM_PACKAGE_NAME);
                     }
 

services/core/java/com/android/server/audio/AudioService.java
@@ -22,10 +22,10 @@ import static android.media.AudioManager.RINGER_MODE_SILENT;
 import static android.media.AudioManager.RINGER_MODE_VIBRATE;
 import static android.media.AudioManager.STREAM_SYSTEM;
 import static android.os.Process.FIRST_APPLICATION_UID;
-import static android.provider.Settings.Secure.VOLUME_HUSH_CYCLE;
-import static android.provider.Settings.Secure.VOLUME_HUSH_MUTE;
-import static android.provider.Settings.Secure.VOLUME_HUSH_OFF;
-import static android.provider.Settings.Secure.VOLUME_HUSH_VIBRATE;
+import static android.provider.Settings.Secure.YAAP_VOLUME_HUSH_NORMAL;
+import static android.provider.Settings.Secure.YAAP_VOLUME_HUSH_MUTE;
+import static android.provider.Settings.Secure.YAAP_VOLUME_HUSH_OFF;
+import static android.provider.Settings.Secure.YAAP_VOLUME_HUSH_VIBRATE;
 
 import static com.android.server.audio.AudioEventLogger.Event.ALOGE;
 import static com.android.server.audio.AudioEventLogger.Event.ALOGI;
@@ -3497,48 +3497,65 @@ public class AudioService extends IAudioService.Stub
     private static Toast mSilenceToast;
 
     public void silenceRingerModeInternal(String reason) {
-        VibrationEffect effect = null;
-        int ringerMode = AudioManager.RINGER_MODE_SILENT;
-        int toastText = 0;
+        String settingsValue = Settings.Secure.getStringForUser(mContentResolver,
+                Settings.Secure.VOLUME_HUSH_GESTURE, UserHandle.USER_CURRENT);
+        if (settingsValue == null) settingsValue = YAAP_VOLUME_HUSH_OFF;
+        final List<String> silenceRingerSetting = Arrays.asList(settingsValue.split(",", 0));
+        if (!mContext.getResources()
+                .getBoolean(com.android.internal.R.bool.config_volumeHushGestureEnabled))
+            return; // disabled by overlay
 
-        int silenceRingerSetting = Settings.Secure.VOLUME_HUSH_OFF;
-        if (mContext.getResources()
-                .getBoolean(com.android.internal.R.bool.config_volumeHushGestureEnabled)) {
-            silenceRingerSetting = Settings.Secure.getIntForUser(mContentResolver,
-                    Settings.Secure.VOLUME_HUSH_GESTURE, VOLUME_HUSH_OFF,
-                    UserHandle.USER_CURRENT);
+        if (settingsValue == YAAP_VOLUME_HUSH_OFF)
+            return; // disabled by user
+
+        // Cycle through enabled modes
+        // Order: normal -> vibrate -> silent
+        SparseArray<String> modeArr = new SparseArray<String>(3);
+        modeArr.put(AudioManager.RINGER_MODE_NORMAL, YAAP_VOLUME_HUSH_NORMAL);
+        modeArr.put(AudioManager.RINGER_MODE_VIBRATE, YAAP_VOLUME_HUSH_VIBRATE);
+        modeArr.put(AudioManager.RINGER_MODE_SILENT, YAAP_VOLUME_HUSH_MUTE);
+
+        int index = -1;
+        if (silenceRingerSetting.size() == 1) {
+            // apply the only enabled mode
+            while (true) {
+                index++;
+                if (settingsValue.equals(modeArr.valueAt(index)))
+                    break;
+            }
+        } else {
+            // apply the next enabled mode
+            boolean found = false;
+            while (true) {
+                index = index < 2 ? index + 1 : 0;
+                if (!found) {
+                    if (mRingerMode == modeArr.keyAt(index))
+                        found = true;
+                    continue;
+                }
+                if (silenceRingerSetting.contains(modeArr.valueAt(index)))
+                    break;
+            }
         }
 
-        switch(silenceRingerSetting) {
-            case VOLUME_HUSH_MUTE:
+        final int ringerMode = modeArr.keyAt(index);
+        int toastText = 0;
+        VibrationEffect effect = null;
+
+        switch(ringerMode) {
+            case AudioManager.RINGER_MODE_SILENT:
                 effect = VibrationEffect.get(VibrationEffect.EFFECT_DOUBLE_CLICK);
-                ringerMode = AudioManager.RINGER_MODE_SILENT;
                 toastText = com.android.internal.R.string.volume_dialog_ringer_guidance_silent;
                 break;
-            case VOLUME_HUSH_VIBRATE:
+            case AudioManager.RINGER_MODE_VIBRATE:
                 effect = VibrationEffect.get(VibrationEffect.EFFECT_HEAVY_CLICK);
-                ringerMode = AudioManager.RINGER_MODE_VIBRATE;
                 toastText = com.android.internal.R.string.volume_dialog_ringer_guidance_vibrate;
                 break;
-            case VOLUME_HUSH_CYCLE:
-                switch (mRingerMode) {
-                    case AudioManager.RINGER_MODE_NORMAL:
-                        effect = VibrationEffect.get(VibrationEffect.EFFECT_HEAVY_CLICK);
-                        ringerMode = AudioManager.RINGER_MODE_VIBRATE;
-                        toastText = com.android.internal.R.string.volume_dialog_ringer_guidance_vibrate;
-                        break;
-                    case AudioManager.RINGER_MODE_VIBRATE:
-                        effect = VibrationEffect.get(VibrationEffect.EFFECT_DOUBLE_CLICK);
-                        ringerMode = AudioManager.RINGER_MODE_SILENT;
-                        toastText = com.android.internal.R.string.volume_dialog_ringer_guidance_silent;
-                        break;
-                    case AudioManager.RINGER_MODE_SILENT:
-                        ringerMode = AudioManager.RINGER_MODE_NORMAL;
-                        toastText = com.android.internal.R.string.volume_dialog_ringer_guidance_normal;
-                        break;
-                }
+            case AudioManager.RINGER_MODE_NORMAL:
+                toastText = com.android.internal.R.string.volume_dialog_ringer_guidance_normal;
                 break;
         }
+
         maybeVibrate(effect, reason);
         setRingerModeInternal(ringerMode, reason);
         if (ringerMode == AudioManager.RINGER_MODE_NORMAL)

yograjfire18/fwb-base-temp

packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
@@ -218,7 +218,22 @@ public class SecureSettingsValidators {
         VALIDATORS.put(Secure.SCREENSAVER_HOME_CONTROLS_ENABLED, BOOLEAN_VALIDATOR);
         VALIDATORS.put(Secure.SHOW_FIRST_CRASH_DIALOG_DEV_OPTION, BOOLEAN_VALIDATOR);
         VALIDATORS.put(Secure.VOLUME_DIALOG_DISMISS_TIMEOUT, NON_NEGATIVE_INTEGER_VALIDATOR);
-        VALIDATORS.put(Secure.VOLUME_HUSH_GESTURE, NON_NEGATIVE_INTEGER_VALIDATOR);
+        VALIDATORS.put(Secure.VOLUME_HUSH_GESTURE,
+                new Validator() {
+                    @Override
+                    public boolean validate(String value) {
+                        if (value.equals(Secure.DERP_VOLUME_HUSH_OFF))
+                            return true;
+                        String[] args = value.split(",", 0);
+                        for (String str : args) {
+                            if (!str.equals(Secure.DERP_VOLUME_HUSH_NORMAL) &&
+                                !str.equals(Secure.DERP_VOLUME_HUSH_MUTE) &&
+                                !str.equals(Secure.DERP_VOLUME_HUSH_VIBRATE))
+                                return false;
+                        }
+                        return true;
+                    }
+                });
         VALIDATORS.put(
                 Secure.ENABLED_NOTIFICATION_LISTENERS,
                 COLON_SEPARATED_COMPONENT_LIST_VALIDATOR); // legacy restore setting

packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
@@ -4563,8 +4563,7 @@ public class SettingsProvider extends ContentProvider {
                             Secure.VOLUME_HUSH_GESTURE);
                     if (currentSetting.isNull()) {
                         secureSettings.insertSettingOverrideableByRestoreLocked(
-                                Secure.VOLUME_HUSH_GESTURE,
-                                Integer.toString(Secure.VOLUME_HUSH_VIBRATE),
+                                Secure.VOLUME_HUSH_GESTURE, Secure.DERP_VOLUME_HUSH_VIBRATE,
                                 null, true, SettingsState.SYSTEM_PACKAGE_NAME);
                     }
 

services/core/java/com/android/server/audio/AudioService.java
@@ -35,10 +35,10 @@ import static android.media.AudioManager.RINGER_MODE_VIBRATE;
 import static android.media.AudioManager.STREAM_SYSTEM;
 import static android.os.Process.FIRST_APPLICATION_UID;
 import static android.os.Process.INVALID_UID;
-import static android.provider.Settings.Secure.VOLUME_HUSH_CYCLE;
-import static android.provider.Settings.Secure.VOLUME_HUSH_MUTE;
-import static android.provider.Settings.Secure.VOLUME_HUSH_OFF;
-import static android.provider.Settings.Secure.VOLUME_HUSH_VIBRATE;
+import static android.provider.Settings.Secure.DERP_VOLUME_HUSH_NORMAL;
+import static android.provider.Settings.Secure.DERP_VOLUME_HUSH_MUTE;
+import static android.provider.Settings.Secure.DERP_VOLUME_HUSH_OFF;
+import static android.provider.Settings.Secure.DERP_VOLUME_HUSH_VIBRATE;
 
 import static com.android.internal.annotations.VisibleForTesting.Visibility.PACKAGE;
 import static com.android.media.audio.Flags.alarmMinVolumeZero;
@@ -5541,48 +5541,65 @@ public class AudioService extends IAudioService.Stub
     private static Toast mSilenceToast;
 
     public void silenceRingerModeInternal(String reason) {
-        VibrationEffect effect = null;
-        int ringerMode = AudioManager.RINGER_MODE_SILENT;
-        int toastText = 0;
+        String settingsValue = Settings.Secure.getStringForUser(mContentResolver,
+                Settings.Secure.VOLUME_HUSH_GESTURE, UserHandle.USER_CURRENT);
+        if (settingsValue == null) settingsValue = DERP_VOLUME_HUSH_OFF;
+        final List<String> silenceRingerSetting = Arrays.asList(settingsValue.split(",", 0));
+        if (!mContext.getResources()
+                .getBoolean(com.android.internal.R.bool.config_volumeHushGestureEnabled))
+            return; // disabled by overlay
 
-        int silenceRingerSetting = Settings.Secure.VOLUME_HUSH_OFF;
-        if (mContext.getResources()
-                .getBoolean(com.android.internal.R.bool.config_volumeHushGestureEnabled)) {
-            silenceRingerSetting = mSettings.getSecureIntForUser(mContentResolver,
-                    Settings.Secure.VOLUME_HUSH_GESTURE, VOLUME_HUSH_OFF,
-                    UserHandle.USER_CURRENT);
+        if (settingsValue == DERP_VOLUME_HUSH_OFF)
+            return; // disabled by user
+
+        // Cycle through enabled modes
+        // Order: normal -> vibrate -> silent
+        SparseArray<String> modeArr = new SparseArray<String>(3);
+        modeArr.put(AudioManager.RINGER_MODE_NORMAL, DERP_VOLUME_HUSH_NORMAL);
+        modeArr.put(AudioManager.RINGER_MODE_VIBRATE, DERP_VOLUME_HUSH_VIBRATE);
+        modeArr.put(AudioManager.RINGER_MODE_SILENT, DERP_VOLUME_HUSH_MUTE);
+
+        int index = -1;
+        if (silenceRingerSetting.size() == 1) {
+            // apply the only enabled mode
+            while (true) {
+                index++;
+                if (settingsValue.equals(modeArr.valueAt(index)))
+                    break;
+            }
+        } else {
+            // apply the next enabled mode
+            boolean found = false;
+            while (true) {
+                index = index < 2 ? index + 1 : 0;
+                if (!found) {
+                    if (mRingerMode == modeArr.keyAt(index))
+                        found = true;
+                    continue;
+                }
+                if (silenceRingerSetting.contains(modeArr.valueAt(index)))
+                    break;
+            }
         }
 
-        switch(silenceRingerSetting) {
-            case VOLUME_HUSH_MUTE:
+        final int ringerMode = modeArr.keyAt(index);
+        int toastText = 0;
+        VibrationEffect effect = null;
+
+        switch(ringerMode) {
+            case AudioManager.RINGER_MODE_SILENT:
                 effect = VibrationEffect.get(VibrationEffect.EFFECT_DOUBLE_CLICK);
-                ringerMode = AudioManager.RINGER_MODE_SILENT;
                 toastText = com.android.internal.R.string.volume_dialog_ringer_guidance_silent;
                 break;
-            case VOLUME_HUSH_VIBRATE:
+            case AudioManager.RINGER_MODE_VIBRATE:
                 effect = VibrationEffect.get(VibrationEffect.EFFECT_HEAVY_CLICK);
-                ringerMode = AudioManager.RINGER_MODE_VIBRATE;
                 toastText = com.android.internal.R.string.volume_dialog_ringer_guidance_vibrate;
                 break;
-            case VOLUME_HUSH_CYCLE:
-                switch (mRingerMode) {
-                    case AudioManager.RINGER_MODE_NORMAL:
-                        effect = VibrationEffect.get(VibrationEffect.EFFECT_HEAVY_CLICK);
-                        ringerMode = AudioManager.RINGER_MODE_VIBRATE;
-                        toastText = com.android.internal.R.string.volume_dialog_ringer_guidance_vibrate;
-                        break;
-                    case AudioManager.RINGER_MODE_VIBRATE:
-                        effect = VibrationEffect.get(VibrationEffect.EFFECT_DOUBLE_CLICK);
-                        ringerMode = AudioManager.RINGER_MODE_SILENT;
-                        toastText = com.android.internal.R.string.volume_dialog_ringer_guidance_silent;
-                        break;
-                    case AudioManager.RINGER_MODE_SILENT:
-                        ringerMode = AudioManager.RINGER_MODE_NORMAL;
-                        toastText = com.android.internal.R.string.volume_dialog_ringer_guidance_normal;
-                        break;
-                }
+            case AudioManager.RINGER_MODE_NORMAL:
+                toastText = com.android.internal.R.string.volume_dialog_ringer_guidance_normal;
                 break;
         }
+
         maybeVibrate(effect, reason);
         setRingerModeInternal(ringerMode, reason);
         if (ringerMode == AudioManager.RINGER_MODE_NORMAL)
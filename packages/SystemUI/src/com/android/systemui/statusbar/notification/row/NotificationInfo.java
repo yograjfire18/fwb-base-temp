Corvus-AOSP/android_frameworks_base

QS Kill button: Remove dependency for toggle
Its not really a view obstruction of any sort, Dont really wanna spam our settings with 69 toggles for nothing

Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
Change-Id: I87d81ae34b1018c289440bba30cce3429673a930

packages/SystemUI/src/com/android/systemui/statusbar/notification/row/NotificationInfo.java
@@ -45,7 +45,6 @@ import android.metrics.LogMaker;
 import android.os.Handler;
 import android.os.RemoteException;
 import android.os.UserHandle;
-import android.provider.Settings;
 import android.service.notification.StatusBarNotification;
 import android.text.TextUtils;
 import android.transition.ChangeBounds;
@@ -324,11 +323,7 @@ public class NotificationInfo extends LinearLayout implements NotificationGuts.G
 
         // Force stop button
         final View killButton = findViewById(R.id.force_stop);
-        boolean killButtonEnabled = Settings.System.getIntForUser(
-                mContext.getContentResolver(),
-                Settings.System.NOTIFICATION_GUTS_KILL_APP_BUTTON, 0,
-                UserHandle.USER_CURRENT) != 0;
-        if (killButtonEnabled && !isThisASystemPackage(mPackageName)) {
+        if (!isThisASystemPackage(mPackageName)) {
             killButton.setVisibility(View.VISIBLE);
             killButton.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {

yograjfire18/fwb-base-temp

packages/SystemUI/src/com/android/systemui/statusbar/notification/row/NotificationInfo.java
@@ -45,7 +45,6 @@ import android.metrics.LogMaker;
 import android.os.Handler;
 import android.os.RemoteException;
 import android.os.UserHandle;
-import android.provider.Settings;
 import android.service.notification.StatusBarNotification;
 import android.text.Html;
 import android.text.TextUtils;
@@ -349,11 +348,7 @@ public class NotificationInfo extends LinearLayout implements NotificationGuts.G
 
         // Force stop button
         final View killButton = findViewById(R.id.force_stop);
-        boolean killButtonEnabled = Settings.System.getIntForUser(
-                mContext.getContentResolver(),
-                Settings.System.NOTIFICATION_GUTS_KILL_APP_BUTTON, 0,
-                UserHandle.USER_CURRENT) != 0;
-        if (killButtonEnabled && !isSystemPackage(mPackageName)) {
+        if (!isSystemPackage(mPackageName)) {
             killButton.setVisibility(View.VISIBLE);
             killButton.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
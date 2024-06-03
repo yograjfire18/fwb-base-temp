LMODroid/platform_frameworks_base

SystemUI: tuner: Add switch to force nav bar menu key
Change-Id: I85907967dbb662965fde5b6f05ad409723508fd4

services/core/java/com/android/server/wm/DisplayPolicy.java
@@ -2335,7 +2335,8 @@ public class DisplayPolicy {
             // the intermediate state to system UI. Otherwise, it might trigger redundant effects.
             return;
         }
-        final boolean forceMenu = false; // TODO user setting?
+        final boolean forceMenu = Settings.Secure.getInt(
+                mContext.getContentResolver(), LMOSettings.Secure.NAV_BAR_FORCE_MENU_KEY, 0) != 0;
         final WindowState navColorWin = chooseNavigationColorWindowLw(mNavBarColorWindowCandidate,
                 mDisplayContent.mInputMethodWindow, mNavigationBarPosition);
         final boolean isNavbarColorManagedByIme =

yograjfire18/fwb-base-temp

services/core/java/com/android/server/wm/DisplayPolicy.java
@@ -2341,7 +2341,8 @@ public class DisplayPolicy {
             // the intermediate state to system UI. Otherwise, it might trigger redundant effects.
             return;
         }
-        final boolean forceMenu = false; // TODO user setting?
+        final boolean forceMenu = Settings.Secure.getInt(
+                mContext.getContentResolver(), Settings.Secure.NAV_BAR_FORCE_MENU_KEY, 0) != 0;
         final WindowState navColorWin = chooseNavigationColorWindowLw(mNavBarColorWindowCandidate,
                 mDisplayContent.mInputMethodWindow, mNavigationBarPosition);
         final boolean isNavbarColorManagedByIme =
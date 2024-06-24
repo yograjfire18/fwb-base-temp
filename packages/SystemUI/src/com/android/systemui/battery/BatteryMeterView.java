99b58b0aa7a1179ec2941d8ed21796c3c7b091fb
Ido Ben-Hur <idoybh2@gmail.com>
2024-06-24 17:55:53 +0300

    BatteryMeterView: Fixup show percentage only when charging
    did not update when charging did.
    Caused by A14/QPR3 mismerge

--- packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
@@ -264,6 +264,7 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
         mThemedDrawable.setBatteryLevel(level);
         mCircleDrawable.setCharging(pluggedIn);
         mCircleDrawable.setBatteryLevel(level);
+        updateShowPercent(); // required for % text only when charging
         updatePercentText();
 
         if (newStatusBarIcons()) {

f15720bbb908350d0e5c914f47f913690229277a
NurKeinNeid <mralexman3000@gmail.com>
2024-08-06 16:50:37 +0200

    fixup! base: Add landscape battery style [1/2]
    To each saint his candle.
    
    Ref: https://github.com/Nusantara-ROM/android_frameworks_base/commit/15b495c8be4c0843abf8929e588060ae430053c0
    
    Change-Id: I608aec0a045b1810ce98ae5d4acf60a16a846b8d
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- packages/SystemUI/src/com/android/systemui/battery/LandscapeBatteryDrawable.kt
+++ packages/SystemUI/src/com/android/systemui/battery/LandscapeBatteryDrawable.kt
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2020-2024 crDroid Android Project
+ * Copyright (C) 2019 The Android Open Source Project
+ * Copyright (C) 2022 Nusantara Android Development
  * Copyright (C) 2024 Yet Another AOSP Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file

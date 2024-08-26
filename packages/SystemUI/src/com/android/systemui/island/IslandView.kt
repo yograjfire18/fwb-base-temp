b05ac831da4ef3a858307f9e507696c1046b576d
minaripenguin <minaripenguin@users.noreply.github.com>
2024-08-26 19:32:30 +0800

    Island: Remove notification when swiped dismiss
    * issue: https://github.com/RisingTechOSS/issue_tracker/issues/19
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/island/IslandView.kt
+++ packages/SystemUI/src/com/android/systemui/island/IslandView.kt
@@ -168,6 +168,11 @@ class IslandView : ExtendedFloatingActionButton {
         this.headsUpManager = headsUp
     }
 
+    private fun removeHun() {
+        val key = headsUpManager?.getTopEntry()?.row?.entry?.key ?: return
+        headsUpManager?.removeNotification(key, true /* releaseImmediately */, false)
+    }
+
     fun showIsland(show: Boolean, expandedFraction: Float) {
         if (show) {
             animateShowIsland(expandedFraction)
@@ -487,6 +492,8 @@ class IslandView : ExtendedFloatingActionButton {
                             translationX = 0f
                             alpha = 1f
                             isDismissed = true
+                            removeHun()
+                            removeInsetsListener()
                             isIslandAnimating = false
                         }
                     } else {
@@ -511,6 +518,8 @@ class IslandView : ExtendedFloatingActionButton {
             visibility = View.GONE
             translationX = 0f
             isDismissed = true
+            removeHun()
+            removeInsetsListener()
             isIslandAnimating = false
         }
         animator.start()

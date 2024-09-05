crdroidandroid/android_frameworks_base

Spoof a valid certificate chain for hardware attestations
I recommend to cherry-pick this in frameworks native: https://github.com/chiteroman/android_frameworks_native/commit/bff5bcfbc52b81d4b1d99acbc84ebfb897346501

keystore/java/android/security/KeyStore2.java
@@ -34,6 +34,8 @@ import android.util.Log;
 
 import java.util.Calendar;
 
+import com.android.internal.util.crdroid.PixelPropsUtils;
+
 /**
  * @hide This should not be made public in its present form because it
  * assumes that private and secret key bytes are available and would
@@ -283,7 +285,9 @@ public class KeyStore2 {
             throws KeyStoreException {
         StrictMode.noteDiskRead();
 
-        return handleRemoteExceptionWithRetry((service) -> service.getKeyEntry(descriptor));
+        KeyEntryResponse response = handleRemoteExceptionWithRetry((service) -> service.getKeyEntry(descriptor));
+
+        return PixelPropsUtils.onGetKeyEntry(response);
     }
 
     /**

yograjfire18/fwb-base-temp

keystore/java/android/security/KeyStore2.java
@@ -34,6 +34,8 @@ import android.util.Log;
 
 import java.util.Calendar;
 
+import com.android.internal.util.derp.PixelPropsUtils;
+
 /**
  * @hide This should not be made public in its present form because it
  * assumes that private and secret key bytes are available and would
@@ -283,7 +285,9 @@ public class KeyStore2 {
             throws KeyStoreException {
         StrictMode.noteDiskRead();
 
-        return handleRemoteExceptionWithRetry((service) -> service.getKeyEntry(descriptor));
+        KeyEntryResponse response = handleRemoteExceptionWithRetry((service) -> service.getKeyEntry(descriptor));
+
+        return PixelPropsUtils.onGetKeyEntry(response);
     }
 
     /**